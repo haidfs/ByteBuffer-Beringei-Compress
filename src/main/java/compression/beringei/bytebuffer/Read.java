5a3d3dab31bd35da58a20d84612bc7c91f260d3a
haidfs <43699312+haidfs@users.noreply.github.com>
2019-05-26 10:19:09 +0800

    Update Read.java

--- src/main/java/compression/beringei/bytebuffer/Read.java
+++ src/main/java/compression/beringei/bytebuffer/Read.java
@@ -1,6 +1,6 @@
 package compression.beringei.bytebuffer;
 public interface Read{
-    //��0����1����ByteBuffer��һ�ζ����bit
+    //read 0 bit,read 1 bit or read many bits
     long readBits(int bitsInValue);
     // Reads a value until the first zero bit is found or limit reached.
     // The zero is included in the value as the least significant bit.
